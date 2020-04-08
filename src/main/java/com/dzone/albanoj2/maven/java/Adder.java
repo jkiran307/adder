7437cef95e537e808128ce5bfa7ceb1612e8c4c7
kiran <jkirankumar307@gmail.com>
2020-04-08 13:58:33 +0530

    Final changes

---
+++ src/main/java/com/dzone/albanoj2/maven/java/Adder.java
@@ -0,0 +1,7 @@
+package com.dzone.albanoj2.maven.java;
+public class Adder {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
+
---
+++ src/main/java/com/dzone/albanoj2/maven/java/Application.java
@@ -0,0 +1,8 @@
+package com.dzone.albanoj2.maven.java;
+public class Application {
+    public static void main(String[] args) {
+        Adder adder = new Adder();
+        System.out.println("3 + 2 = " + adder.add(3, 2));
+    }
+}
+
