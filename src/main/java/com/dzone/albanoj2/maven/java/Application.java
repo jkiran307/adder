fe590b12b65d15ac8973209e7f336ee27f1ad1c9
kiran <jkirankumar307@gmail.com>
2020-04-08 14:13:01 +0530

    1

--- src/main/java/com/dzone/albanoj2/maven/java/Application.java
+++ src/main/java/com/dzone/albanoj2/maven/java/Application.java
@@ -2,7 +2,7 @@ package com.dzone.albanoj2.maven.java;
 public class Application {
     public static void main(String[] args) {
         Adder adder = new Adder();
-        System.out.println("3 + 2 = " + adder.add(3, 2));
+        System.out.println("3 + 2 = " + adder.add(-2, 2));
     }
 }
 
